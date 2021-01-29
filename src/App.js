93ab164753664ffdb47600d1f885c94f92b55367
paszczuh <pawel.nietupski@icloud.com>
2021-01-29 17:58:07 +0100

    killme

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
+import NavBar from './components/layout/NavBar';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import Dashboard from './components/layout/Dashboard';
+import background from './pattern.png';
+import {HashRouter as Router, Route, Switch} from 'react-router-dom';
+import Pokemon from './components/pokemon/Pokemon';
+import { BrowserRouter } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={{background: `url(${background})`}}>
+        <NavBar />
+        <div className='container'>
+          <BrowserRouter>
+            <Switch>
+              <Route exact path="/" component={Dashboard} />
+              <Route exact path="/pokemon/:pokemonIndex" component={Pokemon} />
+              <Dashboard />
+            </Switch>
+          </BrowserRouter>
+        </div>
     </div>
   );
 }
---
+++ src/components/pokemon/PokemonCard.js
@@ -0,0 +1,106 @@
+import React, { Component } from 'react';
+import styled from 'styled-components';
+import spinner from './91.gif';
+import {Link} from 'react-router-dom';
+
+const Sprite = styled.img`
+    width: 5em;
+    height: 5em;
+`;
+
+const Card = styled.div`
+    box-shadow: 0 1px 3px rgba(0, 0, 0, 0.12), 0 1px 2px rgba(0, 0, 0, 0.24);
+    transition: all 0.3s cubic-bezier(0.25, 0.8, 0.25, 1);
+    &:hover {
+        box-shadow: 0 14px 28px rgba(0, 0, 0, 0.25), 0 10px 10px rgba(0, 0, 0, 0.22);
+        -webkit-transform: scale(1.2);
+        -ms-transform: scale(1.2);
+        transform: scale(1.2);
+    };
+    -moz-user-select: none;
+    -website-user-select: none;
+    user-select: none;
+    -o-user-select: none;
+`;
+
+const StyledLink = styled(Link)`
+    text-decoration: none; 
+    color: black;
+    &:focus,
+    &:hover,
+    &:visited,
+    &:link,
+    &:active {
+        text-decoration: none
+    }
+    }
+`;
+
+export default class PokemonCard extends Component {
+
+    state = {
+        name: '',
+        imageUrl: '',
+        pokemonIndex: '',
+        imageLoading: true,
+        tooManyRequests: false,
+    };
+
+    componentDidMount () {
+
+        const {name, url} = this.props;
+        const pokemonIndex = url.split("/")[url.split('/').length - 2];
+        const imageUrl = `./images/${pokemonIndex}.jpg`;
+    
+        this.setState({
+            name, 
+            imageUrl,
+            pokemonIndex
+        });
+    }
+
+    render() {
+
+        return (
+            <div className='col-md-3 col-sm-6 mb-5'>
+                <StyledLink to={`pokemon/${this.state.pokemonIndex}`}>
+                <Card className='card'>
+                    <h5 className="card-header aling-items-center">#{this.state.pokemonIndex}</h5>
+                    {this.state.imageLoading ? (
+                        <img 
+                        alt=""
+                        src={spinner}
+                        style={{width: '5em', height: '5em'}}
+                        className="card-img-top rounded mx-auto d-block mt-2"
+                        ></img>
+                    ) : null}
+                    
+                    <Sprite 
+                        className="card-img-top rounded mx-auto mt-2"
+                        src={this.state.imageUrl}
+                        onLoad={() => this.setState({imageLoading: false})}
+                        onError={() => this.setState({tooManyRequests: true})}
+                        style={
+                            this.state.tooManyRequests
+                            ? { display: 'none' }
+                            : this.state.imageLoading
+                            ? null
+                            : { display: 'block' }
+                        }
+                    />
+                    <div className="card-body mx-auto">
+                        <h6 className="card-title">{this.state.name
+                            .toLowerCase()
+                            .split(" ")
+                            .map(
+                                letter => letter.charAt(0).toUpperCase() + letter.substring(1)
+                                )
+                            .join(' ')}
+                        </h6>
+                    </div>
+                </Card>
+                </StyledLink>
+            </div>
+        )
+    }
+}
